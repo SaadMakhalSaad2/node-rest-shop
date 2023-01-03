6aff1fe19da00ff157829cd565a4f6413e6d2d84
SaadMakhalSaad2 <gosaadmakhal@gmail.com>
2023-01-02 23:59:45 -0600

    handling request errors, automatic reload, logging

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
 
+app.use(morgan("dev"));
+
+//routes for handling different requests
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+
+//reaching this point means none of the above midlewares was requested
+app.use((request, response, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, request, response, next) => {
+  response.status(error.status || 500);
+  response.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 module.exports = app;
