414887497613c406b807abd3b043044d9f8b2797
SaadMakhalSaad2 <gosaadmakhal@gmail.com>
2023-01-03 00:35:50 -0600

    reading data passed in the request

--- api/routes/products.js
+++ api/routes/products.js
@@ -9,8 +9,13 @@ router.get("/", (request, response, next) => {
 });
 
 router.post("/", (request, response, next) => {
+  const product = {
+    name: request.body.name,
+    price: request.body.price,
+  };
   response.status(201).json({
     message: "Handling POST requests to /products",
+    createdProduct: product,
   });
 });
 
