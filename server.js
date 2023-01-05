045ef224de5585d087ffd2d6367325f0e9126402
SaadMakhalSaad2 <gosaadmakhal@gmail.com>
2023-01-04 19:41:16 -0600

    attempting to host 3

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const http = require("http");
-const app = require('./app');
+const app = require("./app");
 
+const hostname = "0.0.0.0";
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
-server.listen(port);
+server.listen(port, hostname, () => {
+  console.log("server running..");
+});
